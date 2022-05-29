222b469b09046dc8cc89c32c909ec6e241e77c8e
AymenBoujmil <56639521+AymenBoujmil@users.noreply.github.com>
2022-05-29 21:00:57 +0100

    add commands

--- contrast.py
+++ contrast.py
@@ -1,73 +1,49 @@
-from IO import *
 from Utils import *
-import random
+from IO import *
 
-def bruit(imageMatrix, ndG, nbLines, nbCols):
-  noiseImage = np.array(imageMatrix)
-  for lines in noiseImage:
-    for cols in range(0,nbCols):
-       n = random.randint(0,20)
-       if(n == 0):
-          lines[cols] = 0
-       if(n == 20):
-          lines[cols] = 255
-  return noiseImage
+def accumulation(imageMatrix, ndG, nbLines, nbCols):
+  h_cum = histogrammeCum(imageMatrix, ndG, nbLines, nbCols)
+  A = [0] * (ndG+1)
+  for i in range(0,ndG+1):
+    pc = h_cum[i] / (nbLines*nbCols)
+    A[i] = int((ndG-1) * pc)
+  return A
 
-def convolution(imageMatrix, ndG, nbLines, nbCols,filter,n):
-  filtredImage = np.array(imageMatrix)
-  for lines in range(0,nbLines):
-    for cols in range(0,nbCols):
-      convolution = 0
-      for i in range(-n//2,(n//2)+ 1):
-        if(((lines + i)>0) and ((lines + i)<nbLines)):
-          for j in range((-n//2),(n//2)+1):
-            if(((cols + j)>0) and ((cols + j)<nbCols)):
-              convolution = convolution + imageMatrix[lines + i][cols + j]*filter[i + n//2][j + n//2]
-      if convolution < 0:
-        convolution = -convolution
-      if convolution > ndG:
-        convolution = ndG
-      filtredImage[lines][cols] = convolution
 
-  return filtredImage
+def egalisation_histogram(imageMatrix, ndG, nbLines, nbCols):
+  A = accumulation(imageMatrix, ndG, nbLines, nbCols)
+  h = histogramme(imageMatrix, ndG, nbLines, nbCols)
+  h_eg = [0]*(ndG+1)
+  for i in range(0, ndG+1):
+    h_eg[A[i]] = h_eg[A[i]] + h[i]
+  return h_eg
 
-def filtreMoyenne(n):
-  filter = np.ones([n,n],dtype=int)
-  filter = 1/(n**2) * filter
-  return filter
+def egalisation_histogram2(imageMatrix, ndG, nbLines, nbCols):
+    histogrammecumm =  histogrammeCum(imageMatrix, ndG, nbLines, nbCols)
+    LUT = [0] * (ndG + 1)
+    n = nbLines * nbCols
+    for i in range(0,ndG + 1):
+        LUT[i] = int(ndG * histogrammecumm[i] / n)
+    imageEqualised  = np.array(imageMatrix)
+    for i in range(0,nbLines):
+        for j in range(0,nbCols):
+            imageEqualised[i][j] = LUT[imageMatrix[i][j]]
+    return imageEqualised
 
-def filterGaussian(n,sigma):
-  gaussian = np.zeros((n, n))
-  m = n//2
-  for x in range(-m, m+1):
-        for y in range(-m, m+1):
-            x1 = 2*np.pi*(sigma**2)
-            x2 = np.exp(-(x**2 + y**2)/(2* sigma**2))
-            gaussian[x+m, y+m] = (1/x1)*x2
-  return gaussian
 
-def filterMedian(imageMatrix, ndG, nbLines, nbCols,n):
-  filtredImage = np.array(imageMatrix)
-  for lines in range(0,nbLines):
-    for cols in range(0,nbCols):
-      medianArray = []
-      for i in range(-n//2,(n//2)+ 1):
-        if(((lines + i)>0) and ((lines + i)<nbLines)):
-          for j in range((-n//2),(n//2)+1):
-            if(((cols + j)>0) and ((cols + j)<nbCols)):
-               medianArray.append(imageMatrix[lines + i][cols + j])
-      medianArray = np.sort(medianArray)
-      length = len(medianArray)
-      filtredImage[lines][cols] = medianArray[length//2]
-  return filtredImage
-
-def filtrePasseHaut(type):
-  if type == 1:
-    return [[0,-1,0],[-1,5,-1],[0,-1,0]]
-  elif type == 2:
-    return [[-1,-1,-1],[-1,9,-1],[-1,-1,-1]]
-  elif type == 3 :
-    return [[1,-2,1],[-2,5,-2],[1,-2,1]]
-  else :
-    return None
-
+def transformation_lineaire(imageMatrix, ndG, nbLines, nbCols,p1,p2):
+  img = np.empty(imageMatrix.shape, dtype=float)
+  for i in range(0,nbLines):
+    for j in range(0,nbCols):
+      if (imageMatrix[i,j] <p1[0]):
+        a = p1[1]/p1[0]
+        img[i,j] = a * imageMatrix[i,j]
+      elif (imageMatrix[i,j] <p2[0]):
+        a = (p2[1]-p1[1])/(p2[0]-p1[0])
+        b = p2[1]-a*p2[0]
+        img[i,j] = a * imageMatrix[i,j] + b
+      else:
+        a = (255 - p2[1])/ (255- p2[0])
+        b = 255 - 255 * a
+        img[i,j] = a * imageMatrix[i,j] + b
+  return img
--- main.py
+++ main.py
@@ -3,22 +3,82 @@ import tkinter as tk
 from tkinter import ttk
 # from PIL import Image, ImageTk
 # from pandas import DataFrame
-# import matplotlib.pyplot as plt
-# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from tkinter.messagebox import *
 from tkinter import *
+from IO import *
+from Utils import *
+from filtres import *
+from Utils import *
+from contrast import *
+(imageMatrix, ndG, nbLines, nbCols) = readImage("./assets/mona.pgm")
 
-# image = readPGM("./assets/portraitWritten.pgm")
+moy = moyenne(imageMatrix, ndG, nbLines, nbCols)
+ecartType = ecartType(imageMatrix, ndG, nbLines, nbCols)
 
+def updateImage(newimage):
+    global imageMatrix
+    global moy
+    global ecartType
+    imageMatrix = newimage
+    im.set_data(imageMatrix)
+    canvas.draw()
+    hist = histogramme(imageMatrix, ndG, nbLines, nbCols)
+    ax1.clear()
+    ax1.plot(hist)
+    bar1.draw()
+
+def reset():
+    global imageMatrix
+    (newImage, ndG, nbLines, nbCols) = readImage("./assets/mona.pgm")
+    imageMatrix = newImage
+    im.set_data(imageMatrix)
+    canvas.draw()
+    hist = histogramme(imageMatrix, ndG, nbLines, nbCols)
+    ax1.clear()
+    ax1.plot(hist)
+    bar1.draw()
+
+
+
+fenetre = Tk()
+image = plt.figure(1,figsize=(5,4))
+im = plt.imshow(imageMatrix, cmap="gray") # later use a.set_data(new_data)
+ax = plt.gca()
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+plt.close(1)
+# a tk.DrawingArea
+
+canvas = FigureCanvasTkAgg(image, master=fenetre)
+canvas.draw()
+canvas.get_tk_widget().grid(row=1, column=1,columnspan=3)
+moyenneL = tk.Button(fenetre, text="Moyenne = " + str(round(moy, 2)), state=tk.DISABLED).grid(row=2, column=0,columnspan=3)
+ecartTypeL = tk.Button(fenetre, text="Ecart Type = " + str(round(ecartType, 2)), state=tk.DISABLED).grid(row=2, column=2,columnspan=3)
+
+hist = histogramme(imageMatrix, ndG, nbLines, nbCols)
+histImage = plt.Figure(figsize=(4,2), dpi=100)
+ax1 = histImage.add_subplot(111)
+
+bar1 = FigureCanvasTkAgg(histImage, fenetre)
+bar1.get_tk_widget().grid(row=3, column=1, columnspan=3)
+ax1.plot(hist)
 
 def alert():
     showinfo("alerte", "Bravo!")
-fenetre = Tk()
+
+
+
+
+
+
 menubar = Menu(fenetre)
 
 menu1 = Menu(menubar, tearoff=0)
 menu1.add_command(label="Ouvrir", command=alert)
 menu1.add_command(label="Enregistrer", command=alert)
+menu1.add_command(label="reset", command=reset)
 menu1.add_separator()
 menu1.add_command(label="Sortir", command=fenetre.quit)
 menubar.add_cascade(label="Fichier", menu=menu1)
@@ -34,11 +94,11 @@ menu2.add_command(label="Inversion de l'image", command=alert)
 menubar.add_cascade(label="Contrast", menu=menu2)
 
 menu3 = Menu(menubar, tearoff=0)
-menu3.add_command(label="Ajouter bruit", command=alert)
-menu3.add_command(label="filtre moyen", command=alert)
-menu3.add_command(label="filtre guassian", command=alert)
-menu3.add_command(label="filtre median", command=alert)
-menu3.add_command(label="filtre passe haut", command=alert)
+menu3.add_command(label="Ajouter bruit", command=lambda : updateImage(bruit(imageMatrix, ndG, nbLines, nbCols)))
+menu3.add_command(label="filtre moyen", command=lambda: updateImage(filterApply(imageMatrix, ndG, nbLines, nbCols,9,"Moynenneur")))
+menu3.add_command(label="filtre guassian", command=lambda : updateImage( filterApply(imageMatrix, ndG, nbLines, nbCols,9,"Gaussian", sigma = 2)))
+menu3.add_command(label="filtre median", command=lambda : updateImage(filterMedian(imageMatrix, ndG, nbLines, nbCols,3)))
+menu3.add_command(label="filtre passe haut", command=lambda : updateImage(filterApply(imageMatrix, ndG, nbLines, nbCols,3,'passeHaut',number = 1)))
 menubar.add_cascade(label="filtres", menu=menu3)
 
 fenetre.config(menu=menubar)
